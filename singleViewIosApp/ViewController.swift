1c1a74650932660b0e5d3873e6b077bde6439c8b
Evgeniy Kiryanov <kiryanoven@gmail.com>
2019-12-08 21:27:08 +0300

    initial commit

---
+++ singleViewIosApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  singleViewIosApp
+//
+//  Created by Evgeniy on 08.12.2019.
+//  Copyright © 2019 Evgeniy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
